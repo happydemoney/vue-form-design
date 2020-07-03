521a9404592dd56bc4c550e276633bd73b52969b
小楠瓜 <happydemoney@126.com>
2020-07-03 20:34:58 +0800

    Update model.vue

--- src/views/form/model.vue
+++ src/views/form/model.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="model-container">
     <draggable
-      v-model="myArray"
+      v-model="formItem"
       :group="{ name: 'people', pull: 'clone', put: false }"
       :sort="false"
       ghostClass='ghost'
       @start="drag=true"
       @end="drag=false"
       :move="handleMove">
-      <div v-for="element in myArray" :key="element.id">{{element.name}}</div>
+      <div v-for="element in formItem" :key="element.prop">{{element.label}}</div>
     </draggable>
   </div>
 </template>
@@ -24,9 +24,70 @@ export default {
   data () {
     return {
       drag: false,
-      myArray: [
-        { id: 1, name: 'input' },
-        { id: 2, name: 'select' }
+      formItem: [
+        {
+          type: "input",
+          prop: "input_" + new Date().getTime(),
+          label: "单行输入框",
+          renderConfig: {
+            defaultValue: ''
+          }
+        },
+        {
+          type: "select",
+          prop: "select_" + new Date().getTime(),
+          label: "下拉选择",
+          renderConfig: {
+            defaultValue: '',
+            options: [
+              { label: '北京', value: 'beijing' },
+              { label: '上海', value: 'shanghai' }
+            ]
+          }
+        },
+        {
+          type: 'radio',
+          label: '单选框',
+          prop: 'radio_' + new Date().getTime(),
+          renderConfig: {
+            defaultValue: '',
+            options: [
+              { label: '高', value: 1 },
+              { label: '中', value: 2 },
+              { label: '低', value: 3 }
+            ]
+          }
+        },
+        {
+          type: 'checkbox',
+          label: '多选框',
+          prop: 'checkbox_' + new Date().getTime(),
+          renderConfig: {
+            defaultValue: [],
+            options: [
+              { label: '美食/餐厅线上活动', value: '美食/餐厅线上活动' },
+              { label: '地推活动', value: '地推活动' },
+              { label: '线下主题活动', value: '线下主题活动' }
+            ]
+          }
+        },
+        {
+          type: "custom",
+          label: "switch切换",
+          prop: "switch_" + new Date().getTime(),
+          renderConfig: {
+            defaultValue: false,
+            name: "ElSwitch"
+          }
+        },
+        {
+          type: "custom",
+          label: "customInput",
+          prop: "customInput_" + new Date().getTime(),
+          renderConfig: {
+            name: "ElInput"
+          }
+        }
       ]
     }
   },
