0a90aa79dec41a2887d910e6b878712e39fd6186
小楠瓜 <happydemoney@126.com>
2020-07-09 19:37:13 +0800

    Update model.vue

--- src/views/form/model.vue
+++ src/views/form/model.vue
@@ -42,7 +42,6 @@ export default {
       formItem: [
         {
           type: "input",
-          prop: "input_" + new Date().getTime(),
           label: "单行输入框",
           renderConfig: {
             defaultValue: ''
@@ -50,46 +49,40 @@ export default {
         },
         {
           type: "select",
-          prop: "select_" + new Date().getTime(),
           label: "下拉选择",
           renderConfig: {
             defaultValue: '',
             options: [
-              { label: '北京', value: 'beijing' },
-              { label: '上海', value: 'shanghai' }
+              { label: 'label_1', value: 'value_1' },
+              { label: 'label_2', value: 'value_2' }
             ]
           }
         },
         {
           type: 'radio',
           label: '单选框',
-          prop: 'radio_' + new Date().getTime(),
           renderConfig: {
             defaultValue: '',
             options: [
-              { label: '高', value: 1 },
-              { label: '中', value: 2 },
-              { label: '低', value: 3 }
+              { label: 'label_1', value: 'value_1' },
+              { label: 'label_2', value: 'value_2' }
             ]
           }
         },
         {
           type: 'checkbox',
           label: '多选框',
-          prop: 'checkbox_' + new Date().getTime(),
           renderConfig: {
             defaultValue: [],
             options: [
-              { label: '美食/餐厅线上活动', value: '美食/餐厅线上活动' },
-              { label: '地推活动', value: '地推活动' },
-              { label: '线下主题活动', value: '线下主题活动' }
+              { label: 'label_1', value: 'value_1' },
+              { label: 'label_2', value: 'value_2' }
             ]
           }
         },
         {
           type: "custom",
           label: "switch切换",
-          prop: "switch_" + new Date().getTime(),
           renderConfig: {
             defaultValue: false,
             name: "ElSwitch"
@@ -98,7 +91,6 @@ export default {
         {
           type: "custom",
           label: "customInput",
-          prop: "customInput_" + new Date().getTime(),
           renderConfig: {
             name: "ElInput"
           }
